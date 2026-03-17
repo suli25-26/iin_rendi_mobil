00331f7c3b5e7e3984547bcf2ee583c4b4162d3b
Gyenge Elemér <gyenge@valahol>
2026-03-17 10:19:05 +0100

    QR-kódolvasás működik

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Reader from './components/Reader';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Reader />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ components/Reader.js
@@ -0,0 +1,68 @@
+
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+import { CameraView, useCameraPermissions } from 'expo-camera'
+
+const Reader = () => {
+    const [permission, requestPermission] = useCameraPermissions()
+    const [scanned, setScanned] = useState(false)
+
+    if(!permission) return <View />
+
+    if(!permission.granted) {
+        return(
+            <View style={styles.container}>
+                <Text>Engedély szükséges a kamerához</Text>
+                <Button 
+                    title="Engedélyez"
+                    onPress={requestPermission}
+                />
+            </View>
+        )
+    }
+
+    const handleBarcodeScanned = ({ type, data }) => {
+        setScanned(true)
+        alert('Üzenet: ' + data)
+    }
+
+
+  return (
+    <View style={styles.container}>
+      <CameraView 
+        style={StyleSheet.absoluteFill}
+        facing="back"
+        onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
+        barcodeScannerSettings={{
+            barcodeTypes: ["qr"],
+        }}
+      />
+      {scanned && (
+        <View style={styles.buttonContainer}>
+            <Button 
+                title="Vissza"
+                onPress={() => setScanned(false)}
+            />
+        </View>
+      )}
+    </View>
+  )
+}
+
+export default Reader
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        justifyContent: "center",
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 50,
+        left: 20,
+        right: 20,
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        borderRadius: 10,
+    },
+})
